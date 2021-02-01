6914b250acd0474b8f8b5a034f464f91c758889a
AbnerAWR <AbnerW_@hotmail.com>
2021-02-01 20:05:06 -0300

    main update

--- vueApp/frontend/src/main.js
+++ vueApp/frontend/src/main.js
@@ -4,11 +4,16 @@ import Vue from 'vue'
 import App from './App'
 
 import './config/bootstrap'
+import './config/msgs'
 import store from './config/store'
+import router from './config/router'
 
 Vue.config.productionTip = false
 
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkFibmVyIFdlc2xleSBSaWJlaXJvIiwiZW1haWwiOiJBYm5lcldfQGhvdG1haWwuY29tIiwiYWRtaW4iOnRydWUsImlhdCI6MTYxMjIwNDQ5MCwiZXhwIjoxNjEyNDYzNjkwfQ.wzNeMKFyTvQhJh1kj7ZPba15Q8n11V7cb8sCVI0W2xc'
+
 new Vue({
   store,
+  router,
   render: h => h(App)
 }).$mount('#app')
