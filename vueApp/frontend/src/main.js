4bc9e3378f2d873a88b904b52ba378602d934634
AbnerAWR <AbnerW_@hotmail.com>
2021-02-03 18:43:01 -0300

    Main update

--- vueApp/frontend/src/main.js
+++ vueApp/frontend/src/main.js
@@ -5,12 +5,12 @@ import App from './App'
 
 import './config/bootstrap'
 import './config/msgs'
+import './config/axios'
+import './config/mq'
 import store from './config/store'
 import router from './config/router'
 
 Vue.config.productionTip = false
-
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkFibmVyIFdlc2xleSBSaWJlaXJvIiwiZW1haWwiOiJBYm5lcldfQGhvdG1haWwuY29tIiwiYWRtaW4iOnRydWUsImlhdCI6MTYxMjIwNDQ5MCwiZXhwIjoxNjEyNDYzNjkwfQ.wzNeMKFyTvQhJh1kj7ZPba15Q8n11V7cb8sCVI0W2xc'
 
 new Vue({
   store,
